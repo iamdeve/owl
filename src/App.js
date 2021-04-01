37adefafcca8bde9b40267f810a7d856bbc6a63b
Abdullah108 <abdullahshahid.ascs@gmail.com>
2021-04-01 12:51:50 +0500

    route delted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomePage from "./Commponents/HomePage";
-import NavBar from "./Commponents/NavBar";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './Commponents/HomePage';
+import NavBar from './Commponents/NavBar';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/Home" component={HomePage} />
-          <Route path="/navbar" component={NavBar} />
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Switch>
+					<Route path='/' component={HomePage} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
